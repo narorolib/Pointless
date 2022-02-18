cd929b8f75fad5dfea81d85cca20bceb660eb7f9
Deinococci <15160167+Deinococci@users.noreply.github.com>
2022-02-18 19:18:26 +0800

    assets

---
+++ public/assets/js/chartjs_defaults.js
@@ -0,0 +1,76 @@
+/* Default chart settings */
+Chart.defaults.global.elements.line.borderWidth = 0;
+Chart.defaults.global.elements.point.radius = 0;
+Chart.defaults.global.elements.point.borderWidth = 0;
+Chart.defaults.global.elements.point.hoverBorderWidth = 0;
+Chart.defaults.global.elements.point.hoverRadius = 0;
+
+let chart_css = window.getComputedStyle(document.body);
+
+/* Default chart options */
+let chart_options = {
+    animation: {
+        duration: 0
+    },
+    hover: {
+        animationDuration: 0,
+    },
+    responsiveAnimationDuration: 0,
+    elements: {
+        line: {
+            tension: 0
+        }
+    },
+    tooltips: {
+        mode: 'x',
+        intersect: false,
+        xPadding: 12,
+        yPadding: 12,
+        titleFontColor: chart_css.getPropertyValue('--dark'),
+        titleSpacing: 30,
+        titleFontSize: 12,
+        titleFontStyle: 'bold',
+        titleMarginBottom: 10,
+        bodyFontColor: chart_css.getPropertyValue('--dark'),
+        bodyFontSize: 12,
+        bodySpacing: 10,
+        backgroundColor: chart_css.getPropertyValue('--light'),
+        footerMarginTop: 10,
+        footerFontStyle: 'normal',
+        footerFontSize: 12,
+        cornerRadius: 4,
+        caretSize: 6,
+    },
+    title: {
+        text: '',
+        display: false
+    },
+    scales: {
+        yAxes: [{
+            display: false,
+            gridLines: {
+                display: false
+            },
+            ticks: {
+                beginAtZero: true,
+                userCallback: (value, index, values) => {
+                    if (Math.floor(value) === value) {
+                        return nr(value);
+                    }
+                },
+            }
+        }],
+        xAxes: [{
+            gridLines: {
+                display: false
+            },
+            display: false,
+            ticks: {
+                maxTicksLimit:  15,
+                padding: 10,
+            },
+        }]
+    },
+    responsive: true,
+    maintainAspectRatio: false
+};
